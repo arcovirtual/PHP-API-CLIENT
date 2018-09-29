8190edf5592591978345469c3b85d6c0a547bb5f
arcovirtual <33576632+arcovirtual@users.noreply.github.com>
2018-09-29 06:47:03 -0300

    mostrar valores en html
    deberia funcionar el ejemplo sencillo para mostrar los resultados en formato html y asi podran ir generando su propio comprobante de pago. fueron varias horas leyendo y viendo videos de API REST JSON etc para comprender como funciona..- si me equivoco me avisan.

--- examples/payments/result.php
+++ examples/payments/result.php
@@ -22,6 +22,12 @@ try {
 	$response = $flowApi->send($serviceName, $params, "GET");
 	
 	print_r($response);
+// resultado $response = array(9) { ["flowOrder"]=> int(35779) ["commerceOrder"]=> string(4) "1961" 
+// recorremos el array resultado	
+	foreach($response as $flowordervalor => $ordennumero)
+	{ echo  "Numero de Orden: <b> $ordennumero </b> "; }
+	// y asi siguen extrayendo info no se si se podran
+	//usar las variables nuevas como $ordennumero para ser usada en otras integraciones deberia poder.
 	
 } catch (Exception $e) {
 	echo "Error: " . $e->getCode() . " - " . $e->getMessage();
